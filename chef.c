9c9decdcee1a6ef08daddb9c581f0c8ec312b58c
Sonny-Klotz <syk_778@hotmail.fr>
2016-04-10 22:10:24 +0200

    ecriture structure globale fichier

--- chef.c
+++ chef.c
@@ -1,32 +1,82 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
 #include "fonctions.h"
 
-/*
- int main (int argc, char* argv[])
-{
-	int num[N], i;
-	pthread_t thr[N];	//tableau de N thread
-	for (i = 0; i < N; i++)
-	{//creer le thread
-		num[i] = i+1;
-		pthread_create(&thr[i],NULL,affichage,(void*)&num[i]);
-	}
-	void* join;
-	for (i = 0; i < N; i++)
-	{
-		pthread_join(thr[i],&join);
-		printf("%d\n",*(int*)(join));
-		free(join);
-	}
-	return 0;
-}
-code pour creer un thread utilisee par le prof
+//changer l'emplacement de la déclaration (ne laisser que dans un .h)
+enum{MIN, MAX, SUM, AVG, ODD};
 
-struct inf{
-	int debut;		//indices dans le tableau de ou à ou chercher le max
-	int fin;		//indice fin
-	int retour;		//stocker le max
-	pthread_t thr;	//entier, avoir l'info du thread en cours
+int creaEmployes(void *(*fct) (void *), int nb_thr, void *arg)
+{
+	int i;
+	pthread_t thr[nb_thr];
+	
+	for (i = 0; i < nb_thr; i++)
+	{
+		//maj indices de arg a ajouter
+		pthread_create(thr + i, NULL, fct, arg);
+		//arg est modifie par les threads
+		//besoin mutex pour proteger resultat dans fonctions
+	}
+	
+	//possibilite ajout var dans join, pour eventuelles erreurs
+	for (i = 0; i < nb_thr; i++)
+		pthread_join(thr[i], NULL);
+		
+	//return getResultat((struct donnee *) arg);
 }
-est ce possible de mettre debut et fin pointeurs sur des element du fichier?
-sinon est ce possible de remplir un tableau a partir de chaque fichier ?
- */
+
+void chef(char *cheminFic, char *cmd)
+{
+	//Ouverture du fichier donné en argument
+	int fd = open(cheminFic, O_RDONLY);
+	if(fd < 0)
+	{
+		fprintf(stderr, "Erreur à l'ouverture du fichier %s\n", cheminFic);
+		exit(EXIT_FAILURE);
+	}
+	
+	//recuperation taille fichier, determination nbre thread à utiliser
+	int nb_thr = 0;
+	//a faire proprement
+	void *arg = NULL;
+	//struct donnee *arg = init_struct();
+	
+	//recup commande, lancement threads
+	switch(recherche_operation(cmd))
+	{
+		//ne compile pas encore car les fonctions.h ne sont pas encore au format void * fct(void *)
+		case(MIN) : creaEmployes(min, nb_thr, arg); break;
+		case(MAX) : creaEmployes(max, nb_thr, arg); break;
+		case(SUM) : creaEmployes(sum, nb_thr, arg); break;
+		case(AVG) : creaEmployes(avg, nb_thr, arg); break;
+		case(ODD) : creaEmployes(odd, nb_thr, arg); break;
+		default :
+		{
+			fprintf(stderr, "Erreur commande non trouvée %s\n", cmd);
+			break;
+		}
+	}	
+	
+	//Retour au proc directeur la solution du fichier + nb élémemnts (avg)
+	//Communication par pipes ?
+	
+	//libération des ressources, terminaison du processus
+	//au cas ou on malloc pour la structure
+	//libereMem(arg);
+	close(fd);
+	exit(EXIT_SUCCESS);
+}
+
+/* struct inf{
+		int debut;		//indices dans le tableau de ou à ou chercher le max
+		int nbElt;		//nombre éléments
+		int resultat;	//stocker le résultat
+		pthread_t thr;	//entier, avoir l'info du thread en cours
+		int fd;			//descripteur du fichier utilisé
+} */
--- chef.h
+++ chef.h
@@ -1,5 +1,25 @@
 #ifndef DEF_DEF
 #define DEF_DEF
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
 #include "fonctions.h"
 
 #endif
+
+enum{MIN, MAX, SUM, AVG, ODD};
+
+/* Fonction qui va créer nb_thr threads exécutant
+ * la commande fct, et retourne la solution*/
+int creaEmployes(void *(*fct) (void *), int nb_thr, void *arg)
+
+/* Fonction principale du fichier chef
+ * Effectue ce que doit faire un processus chef
+ * à partir d'un fichier et d'une commande donnée*/
+void chef(char *cheminFic, char *cmd);
