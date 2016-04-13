3ebc77b5bb6e2f70f9aebbac4af8a07c52c7e969
Sonny Klotz <syk_778@hotmail.fr>
2016-04-13 22:03:40 +0200

    Update donnees.h
    Retrait du nb_element et mutex correspondant, ce sera gérer directement dans les fonctions (plus clair)

--- donnees.h
+++ donnees.h
@@ -18,11 +18,10 @@ enum{MIN, MAX, SUM, AVG, ODD};
 
 struct inf{
 	int fd;		//pointeur sur le debut (a part  la premiere ligne a gerer par sonny)
-	double retour;		//stocker le resultat du calcul
 	pthread_t thr;	//entier, avoir l'info du thread en cours
+	double retour;		//stocker le resultat du calcul
 	pthread_mutex_t mut_fic; //pour protéger la lecture du fichier
 	pthread_mutex_t mut_ret; //pour protéger la valeur de retour
-	pthread_mutex_t mut_nb;  //pour protéger le nb_elements
 };
 typedef struct inf inf;
 
