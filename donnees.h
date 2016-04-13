36f701ff93d3cb2bcb15b4a0031f818137b6e0a2
Sonny Klotz <syk_778@hotmail.fr>
2016-04-13 22:02:41 +0200

    Update donnees.h
    champ nb_elements retiré, on comptera directement dans les fonctions

--- donnees.h
+++ donnees.h
@@ -19,7 +19,6 @@ enum{MIN, MAX, SUM, AVG, ODD};
 struct inf{
 	int fd;		//pointeur sur le debut (a part  la premiere ligne a gerer par sonny)
 	double retour;		//stocker le resultat du calcul
-	int nb_elements;
 	pthread_t thr;	//entier, avoir l'info du thread en cours
 	pthread_mutex_t mut_fic; //pour protéger la lecture du fichier
 	pthread_mutex_t mut_ret; //pour protéger la valeur de retour
