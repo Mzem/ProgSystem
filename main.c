c7a71a5ebc6696fb64a5a72176464ea9d5e483b7
Sonny Klotz <syk_778@hotmail.fr>
2016-04-13 21:30:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include "directeur.h"
-#include "donnees.h"
-
-
 
 int main(int argc, char* argv[]) 
 {
 	int n=argc-2;//nombre de fichiers
 	if(argc < 3) //pas d'operation ou pas de fichier
-		{
-			printf("le nombre d'arguments est insuffisant\n");
-			return 0 ;
-		}
-	return n ;
+	{
+		fprintf(stderr, "le nombre d'arguments est insuffisant\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(!fork())
+	{
+		//directeur( arguments eventuels ); fonction principale de malek
+	}
+	else
+	{
+		wait(NULL); //argument éventuel pour traitement de l'erreur
+	}
+	
+	return 0;
 }
